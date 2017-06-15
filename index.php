90494e39d3328b657468d4ee43c414e48febb80d
bunkermaster <yann.le-scouarnec@hetic.net>
2017-06-15 13:03:39 +0200

    +liste des livres

--- index.php
+++ index.php
@@ -7,3 +7,26 @@
  */
 require_once __DIR__."/conf.php";
 require_once APP_DIR_INC."connect.php";
+$sql = "SELECT `id`, `titre`, `resume` FROM `livre`;";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+if ($stmt->errorCode() !== "00000") {
+    die($stmt->errorInfo()[2]);
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Liste des livres</title>
+</head>
+<body>
+<table>
+<?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)):?>
+    <tr>
+        <td><?=$row['titre']?></td>
+    </tr>
+<?php endwhile; ?>
+</table>
+</body>
+</html>
