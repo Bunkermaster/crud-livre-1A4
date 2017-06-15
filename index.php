8a313fe5ec424a16c01c0d1d361ea0d2d0a5ff22
bunkermaster <yann.le-scouarnec@hetic.net>
2017-06-15 13:06:55 +0200

    commentaires

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
  * Date: 15/06/2017
  * Time: 11:57
  */
+// chargement de la configuration
 require_once __DIR__."/conf.php";
+// connexion à la base de données
 require_once APP_DIR_INC."connect.php";
+// requete de liste des livres
 $sql = "SELECT `id`, `titre`, `resume` FROM `livre`;";
+// preparation requete
 $stmt = $pdo->prepare($sql);
+// execution requete
 $stmt->execute();
+// gestion des erreurs
 if ($stmt->errorCode() !== "00000") {
     die($stmt->errorInfo()[2]);
 }
